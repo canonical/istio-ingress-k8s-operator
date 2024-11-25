ebcea8709c3b19b52aefe9a2eb65204e4db06d95
michael <michael.dmitry98@gmail.com>
2024-11-25 10:41:05 +0200

    add charm tracing

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,7 +6,14 @@
 from unittest.mock import patch
 
 import pytest
+from charms.tempo_coordinator_k8s.v0 import charm_tracing
 from lightkube import Client
+
+
+@pytest.fixture(autouse=True)
+def charm_tracing_buffer_to_tmp(tmp_path):
+    with patch.object(charm_tracing, "BUFFER_DEFAULT_CACHE_FILE_NAME", tmp_path):
+        yield
 
 
 @pytest.fixture(autouse=True)
